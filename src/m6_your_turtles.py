43ff0d3acc2222025b17a690d88d30daaf921542
Carla Archuleta <archulca@rose-hulman.edu>
2019-01-17 16:42:23 -0500

    TODO #1

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -19,7 +19,33 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #     2. Each rg.SimpleTurtle object draws something
 #          (by moving, using its rg.Pen).  ANYTHING is fine!
 #     3. Each rg.SimpleTurtle moves inside a LOOP.
-#
+import rosegraphics as rg
+window = rg.TurtleWindow()
+dean = rg.SimpleTurtle()
+kate = rg.SimpleTurtle()
+dean.pen = rg.Pen('purple',5)
+kate.pen = rg.Pen('blue',10)
+dean.speed = 10
+kate.speed = 50
+size = 100
+for k in range(5):
+    dean.draw_circle(size)
+    dean.pen_up()
+    dean.forward(50)
+    dean.right(50)
+    dean.pen_down()
+    size = size - 15
+kate.left(100)
+size1 = 100
+for k in range(30):
+    kate.draw_regular_polygon(8,size1)
+    kate.pen_up()
+    kate.backward(100)
+    kate.left(100)
+    kate.pen_down()
+    size1 = size1 - 2
+window.close_on_mouse_click()
+
 #   Be creative!  Strive for way-cool pictures!  Abstract pictures rule!
 #
 #   If you make syntax (notational) errors, no worries -- get help
